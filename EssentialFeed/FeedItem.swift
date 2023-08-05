9713282dcb635e9c9c7151d5ced5d9e783257579
merteyup <eupmrt@gmail.com>
2023-08-05 13:42:19 +0300

    Create the Feed Item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Eyüp Mert on 5.08.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let it: UUID
+    let description : String?
+    let location: String?
+    let imageURL: URL
+    
+}
+
