7af346d7c3691b837d29bd104801a1c94e3bc3ce
merteyup <eupmrt@gmail.com>
2023-08-06 15:54:46 +0300

    Move remote feed loader to production

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -7,40 +7,14 @@
 
 import Foundation
 import XCTest
-
-
-class RemoteFeedLoader {
-    
-    let client : HTTPClient
-    let url: URL
-     
-    init(url: URL , client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-    
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
-
+import EssentialFeed
 
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        
-        let url = URL(string: "https://a-url.com")!
-
         let (_ , client) = makeSUT()
-            
         XCTAssertNil(client.requestedURL)
-        
     }
     
     func test_load_requestedDataFromURL() {
