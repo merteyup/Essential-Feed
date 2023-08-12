546e93d2b997682916a219a7e3ed8265da5a8dff
merteyup <eupmrt@gmail.com>
2023-08-12 20:30:52 +0300

    End to end test timout duration change

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -49,7 +49,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 10.0)
+        wait(for: [exp], timeout: 15.0)
         return receivedResult
     }
     
