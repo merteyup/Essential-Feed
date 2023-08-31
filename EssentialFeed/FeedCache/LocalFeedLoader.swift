7c2de59a3d7de1a84a09360aa0222f79008e281e
merteyup <eupmrt@gmail.com>
2023-08-31 16:46:33 +0300

    Replace load command completion to return a result type rather than an optional error

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -14,6 +14,8 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
     
     public typealias SaveResult = Error?
+    public typealias LoadResult = LoadFeedResult
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -30,8 +32,12 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve{ error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }
     }
     
     
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -26,24 +26,27 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     }
     
     func test_load_failsOnRetrievalError() {
-        
         let (sut, store) = makeSUT()
         let retrievalError = anyNSError()
         let exp = expectation(description: "Wait for load completion")
         
-        
         var receivedError: Error?
-        sut.load{ error in
-            receivedError = error
+        sut.load{ result in
+            
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure, got \(result) instead")
+            }
             exp.fulfill()
         }
         
         store.completeRetrieval(with: retrievalError)
         wait(for: [exp], timeout: 1.0)
         XCTAssertEqual(receivedError as NSError?, retrievalError)
-        
     }
-    
+        
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
