69772097597ae5b8793342be92bf135e7770ce13
merteyup <eupmrt@gmail.com>
2023-09-11 12:55:37 +0300

    Implemented test_insert_deliversNoErrorOnNonEmptyCache to the 'CoreDataFeedStoreTests'

--- EssentialFeedTests/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/CoreDataFeedStoreTests.swift
@@ -37,7 +37,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
