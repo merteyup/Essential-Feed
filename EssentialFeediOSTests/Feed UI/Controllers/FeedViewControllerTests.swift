3460cecf17d13f2f7b4c07ccf1cecc5298025cab
merteyup <eupmrt@gmail.com>
2023-10-17 15:56:48 +0300

    Extract UIImage test helper to a separate file

--- EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -460,16 +460,3 @@ private extension UIRefreshControl {
         }
     }
 }
-
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-        
-        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-            color.setFill()
-            rendererContext.fill(rect)
-        }
-    }
-}
