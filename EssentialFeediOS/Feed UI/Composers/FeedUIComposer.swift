390765d3e01825909b8c9b7fd4ad52aece51422f
merteyup <eupmrt@gmail.com>
2023-10-24 18:34:45 +0300

    Dispatch background feed image data completion to main thread before passing result to the UI components. Threading is dealt with a Decorator in the Composition layer.

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -20,7 +20,9 @@ public final class FeedUIComposer {
             title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(
-            feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+            feedView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(feedController))
         return feedController
     }
@@ -37,6 +39,8 @@ private final class MainQueueDispatchDecorator<T> {
         guard Thread.isMainThread else {
             return DispatchQueue.main.async(execute: completion)
         }
+        
+        completion()
     }
 }
 
@@ -50,6 +54,13 @@ extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
     }
 }
 
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
 
 
 private extension FeedViewController {
@@ -96,13 +107,13 @@ private final class FeedViewAdapter: FeedView {
     func display(_ viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map { model in
             let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
-                    let view = FeedImageCellController(delegate: adapter)
-
-                    adapter.presenter = FeedImagePresenter(
-                        view: WeakRefVirtualProxy(view),
-                        imageTransformer: UIImage.init)
-
-                    return view
+            let view = FeedImageCellController(delegate: adapter)
+            
+            adapter.presenter = FeedImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init)
+            
+            return view
         }
     }
 }
@@ -135,29 +146,29 @@ private final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, 
     private let model: FeedImage
     private let imageLoader: FeedImageDataLoader
     private var task: FeedImageDataLoaderTask?
-
+    
     var presenter: FeedImagePresenter<View, Image>?
-
+    
     init(model: FeedImage, imageLoader: FeedImageDataLoader) {
         self.model = model
         self.imageLoader = imageLoader
     }
-
+    
     func didRequestImage() {
         presenter?.didStartLoadingImageData(for: model)
-
+        
         let model = self.model
         task = imageLoader.loadImageData(from: model.url) { [weak self] result in
             switch result {
             case let .success(data):
                 self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-
+                
             case let .failure(error):
                 self?.presenter?.didFinishLoadingImageData(with: error, for: model)
             }
         }
     }
-
+    
     func didCancelImageRequest() {
         task?.cancel()
     }
