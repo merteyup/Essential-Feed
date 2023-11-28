1cc548dfa8f903e65cc2d5eb48fd06e4937797c8
merteyup <eupmrt@gmail.com>
2023-11-28 09:24:58 +0300

    Remove unused test case

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteWithLocalFallbackFeedLoaderTests.swift
+//  EssentialAppTests
+//
+//  Created by Eyüp Mert on 28.11.2023.
+//
+
+import XCTest
+
+class RemoteWithLocalFallbackFeedLoaderTests: XCTestCase {
+    
+    
+}
