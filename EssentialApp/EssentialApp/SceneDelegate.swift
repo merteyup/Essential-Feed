9ddbc478b05f0d65ca9aebbceca86784b5ab73d8
merteyup <eupmrt@gmail.com>
2023-12-03 10:29:04 +0300

    Add #if DEBUG compilation directive to prevent test-specific code from being deployed in production

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -28,9 +28,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
         
+#if DEBUG
         if CommandLine.arguments.contains("-reset") {
             try? FileManager.default.removeItem(at: localStoreURL)
         }
+#endif
         
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -50,15 +52,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+#if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
             return AlwaysFailingHTTPClient()
-            
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         }
+#endif
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
     
+#if DEBUG
     private class AlwaysFailingHTTPClient: HTTPClient {
         private class Task: HTTPClientTask {
             func cancel() {}
@@ -69,6 +71,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return Task()
         }
     }
+#endif
     
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
