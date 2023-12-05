48372ffc9a0f514638216df55d996f584d7d7faa
merteyup <eupmrt@gmail.com>
2023-12-05 16:01:15 +0300

    Record feed with error message snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -20,12 +20,20 @@ class FeedSnapshotTests: XCTestCase {
     }
     
     func test_feedWithContent() {
-            let sut = makeSUT()
-
-            sut.display(feedWithContent())
-
-            record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
-        }
+        let sut = makeSUT()
+        
+        sut.display(feedWithContent())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+    }
+    
+    func test_feedWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+    }
     
     // MARK: - Helpers
     private func makeSUT() -> FeedViewController {
@@ -37,19 +45,19 @@ class FeedSnapshotTests: XCTestCase {
     }
     
     private func feedWithContent() -> [ImageStub] {
-            return [
-                ImageStub(
-                    description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                    location: "East Side Gallery\nMemorial in Berlin, Germany",
-                    image: UIImage.make(withColor: .red)
-                ),
-                ImageStub(
-                    description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                    location: "Garth Pier",
-                    image: UIImage.make(withColor: .green)
-                )
-            ]
-        }
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
     
     private func emptyFeed() -> [FeedImageCellController] {
         return []
@@ -102,7 +110,7 @@ private extension FeedViewController {
 private class ImageStub: FeedImageCellControllerDelegate {
     let viewModel: FeedImageViewModel<UIImage>
     weak var controller: FeedImageCellController?
-
+    
     init(description: String?, location: String?, image: UIImage?) {
         viewModel = FeedImageViewModel(
             description: description,
@@ -111,10 +119,10 @@ private class ImageStub: FeedImageCellControllerDelegate {
             isLoading: false,
             shouldRetry: image == nil)
     }
-
+    
     func didRequestImage() {
         controller?.display(viewModel)
     }
-
+    
     func didCancelImageRequest() {}
 }
