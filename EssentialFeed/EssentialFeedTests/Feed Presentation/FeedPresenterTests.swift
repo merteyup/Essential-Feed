11ff4c6d6012126504ec3bc9dcc48e71bf271fbb
merteyup <eupmrt@gmail.com>
2024-01-09 11:37:05 +0300

    Replace `FEED_VIEW_CONNECTION_ERROR`with `GENERIC_CONNECTION_ERROR`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -51,7 +51,7 @@ class FeedPresenterTests: XCTestCase {
         
         sut.didFinishLoadingFeed(with: anyNSError())
         
-        XCTAssertEqual(view.messages, [.display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")), .display(isLoading: false)])
+        XCTAssertEqual(view.messages, [.display(errorMessage: localized("GENERIC_CONNECTION_ERROR")), .display(isLoading: false)])
     }
     
     // MARK: - Helpers
