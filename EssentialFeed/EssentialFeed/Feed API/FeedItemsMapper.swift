2e7e2b78f2d1ed3018f1199ac9f560ebd402dd82
merteyup <eupmrt@gmail.com>
2024-03-16 21:35:45 +0300

    Move RemoteLoaderComposition to the composition root.

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -39,10 +39,14 @@ public final class FeedItemsMapper {
                             url: image)
         }
     }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
         
     public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
         guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            throw RemoteFeedLoader.Error.invalidData
+            throw Error.invalidData
         }
         return root.images
     }
